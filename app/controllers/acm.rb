fe1acf2c2501b0872a4722295f71338ffcc41dad
Tom Dooner <tom@causes.com>
2012-09-08 16:09:53 -0400

    Add first-draft copy to ACM home page
    This adds a first-cut of copy to ACM's index page, at /acm.
    
    This also creates a stub for a page at /acm/servers, which I will fill
    in subsequently with Brian's ACM Server announcement e-mail.

--- app/controllers/acm.rb
+++ app/controllers/acm.rb
@@ -1,9 +1,13 @@
 Cabal.controller :acm do
   get :index do
+    @acm_conference_url = "http://#{request.host_with_port}#{url_for(:acm, :conference)}"
     render 'acm/index'
   end
 
   get :conference do
     render 'acm/conference'
+  end
+
+  get :servers do
   end
 end
