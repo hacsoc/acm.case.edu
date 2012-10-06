05a829390d36052a5fc187f97089e435c28ded4d
Brian Stack <bis12@case.edu>
2012-10-06 17:11:42 +0000

    title

--- app/controllers/acm.rb
+++ app/controllers/acm.rb
@@ -1,10 +1,13 @@
 Cabal.controller :acm do
+  
   get :index, :map => '/' do
     @acm_conference_url = "http://#{request.host_with_port}#{url_for(:acm, :conference)}"
+    @name = 'Link-State Signup'
     render 'acm/index'
   end
 
   get :conference do
+    @name = 'Link-State'
     render 'acm/conference'
   end
 
